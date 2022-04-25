b688a3c16786647476fc65ee9fd927ddb1217d05
sbaisden25 <spbaisden@gmail.com>
2022-04-25 05:23:44 -0400

    test

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var app = express();
 
+const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET",
+    "Access-Control-Allow-Headers": "Content-Type",
+};
+
 const PORT = process.env.PORT || 8080;
 
 const articles = require("./articles.json");
@@ -21,7 +27,8 @@ app.get("/", (req, res) => {
 
     const article = url + random_article()
     const json = JSON.stringify({ article: article})
-    res.send(json);
+    res.send(json,
+        headers);
     
 })
 
