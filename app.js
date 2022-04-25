d59d1f3ababf1713ce93bb44228774199f197242
sbaisden25 <spbaisden@gmail.com>
2022-04-25 04:55:11 -0400

    4:55am testin

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 var express = require("express");
 var app = express();
-var cors = require('cors');
 
-app.use(
-    cors({
-        credentials: true,
-        origin: true
-    })
-);
-app.options('*', cors());
+const PORT = process.env.PORT || 8080;
 
 const articles = require("./articles.json");
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
  console.log("Server running on port 3000");
 });
 
